5e9bbc5cc7997998062219b055e214f0e81a0921
vedmandal <151810723+vedmandal@users.noreply.github.com>
2026-02-06 22:41:32 +0530

    fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api"
+  baseURL: process.env.REACT_APP_API_BASE_URL
 });
 
 api.interceptors.request.use(config => {
